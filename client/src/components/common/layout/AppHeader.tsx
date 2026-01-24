e8e03a5c704c2a6f86c0a30adf0a72fe4555b1e4
jordan-dalby <jordan.dalby1@outlook.com>
2026-01-24 17:02:21 +0000

    [feat] removed logo from the header

--- client/src/components/common/layout/AppHeader.tsx
+++ client/src/components/common/layout/AppHeader.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { APP_VERSION } from '../../../constants/settings';
-import { getAssetPath } from '../../../utils/paths';
 
 interface AppHeaderProps {
   subtitle?: string;
@@ -12,8 +11,7 @@ export const AppHeader: React.FC<AppHeaderProps> = ({ subtitle, children }) => {
   return (
     <div className="flex flex-col gap-2">
       <h1 className="text-4xl font-bold text-light-text dark:text-dark-text flex items-baseline gap-2">
-        <Link to="/" className="flex items-baseline gap-2 hover:opacity-80 transition-opacity cursor-pointer">
-          <img src={getAssetPath('/logo512.png')} alt="ByteStash Logo" className="w-7 h-7" />
+        <Link to="/" className="hover:opacity-80 transition-opacity cursor-pointer">
           ByteStash
         </Link>
         <span className="text-sm text-light-text-secondary dark:text-dark-text-secondary">v{APP_VERSION}</span>
